fb426a4b0c972899ca58506959295fbd54f250f6
Edward Smale <essmale2005@gmail.com>
2021-05-07 22:28:00 +0100

    Add default config file

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Hindenburg
-
 ### WIP
 Hindenburg is currently not in a particularly usable state, so it's recommended not to use it right now.
 
-Hindenburg is a performant and scalable Among Us servers, complete with plugins.
+# Hindenburg
+
+Hindenburg is a completely customisable, performant and scalable Among Us server, complete with plugins and a sophisticated and configurable anti-cheat.
 
 It uses [SkeldJS](https://github.com/SkeldJS/SkeldJS) to interface with the Among Us protocol, as well as keeping state.
--- bin/load-balancer.ts
+++ bin/load-balancer.ts
@@ -5,9 +5,56 @@ import { HindenburgConfig, HindenburgLoadBalancer } from "../src";
 
 
 (async () => {
-    const config = JSON.parse(await fs.readFile(path.resolve(process.cwd(), "./config.json"), "utf8")) as HindenburgConfig;
+    const config_path = path.resolve(process.cwd(), "./config.json");
 
-    const server = new HindenburgLoadBalancer(config);
+    try {
+        const config = JSON.parse(await fs.readFile(config_path, "utf8")) as HindenburgConfig;
 
-    server.listen();
+        const server = new HindenburgLoadBalancer(config);
+
+        server.listen();
+    } catch (e) {
+        console.log("No config file detected, writing default config..");
+
+        const config = `{
+    "$schema": "./misc/config.schema.json",
+    "serverName": "Hindenburg",
+    "serverVersion": "1.0.0",
+    "reactor": false,
+    "anticheat": {
+        "checkSettings": true,
+        "maxConnectionsPerIp": 2,
+        "checkObjectOwnership": true,
+        "hostChecks": true,
+        "malformedPackets": true,
+        "massivePackets": {
+            "penalty": "disconnect",
+            "strikes": 3
+        },
+        "versions": ["2021.4.2"]
+    },
+    "node": {
+        "ip": "127.0.0.1",
+        "port": 22123
+    },
+    "master": {
+        "nodes": [
+            {
+                "ip": "127.0.0.1",
+                "port": 22123
+            }
+        ],
+        "ip": "127.0.0.1",
+        "port": 22023
+    },
+    "redis": {
+        "host": "127.0.0.1",
+        "port": 6379
+    }
+}`;
+
+        await fs.writeFile(config_path, config, "utf8");
+        
+        console.log("Wrote default config file, please restart the program to continue.");
+    }
 })();

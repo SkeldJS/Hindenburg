f54586bf2106857358bd774fa010440c745a21e3
Edward Smale <essmale2005@gmail.com>
2021-06-22 10:38:13 +0100

    Update setup.js

--- bin/setup.js
+++ bin/setup.js
@@ -10,6 +10,12 @@ function doesExist(path) {
     }
 }
 
+const default_package_json = `{
+    "dependencies": {
+        "hbplugin-ban-textfile": "next"
+    }
+}`
+
 const default_config = `{
     "plugins": {}
 }`;
@@ -21,9 +27,8 @@ if (!doesExist(plugins_dir)) {
     console.log("No plugins directory, creating folder, yarn.lock and package.json.");
     fs.mkdirSync(plugins_dir);
     fs.writeFileSync(path.join(plugins_dir, "yarn.lock"), "", "utf8");
-    fs.writeFileSync(path.join(plugins_dir, "package.json"), `{
-    "dependencies": {}
-}`);
+    fs.writeFileSync(path.join(plugins_dir, "package.json"), default_package_json);
+    console.log("Created plugins/ at " + plugins_dir);
 } else {
     if (!doesExist(path.join(plugins_dir, "yarn.lock"))) {
         console.log("No yarn.lock for plugins, creating one.");
@@ -31,13 +36,15 @@ if (!doesExist(plugins_dir)) {
     }
     if (!doesExist(path.join(plugins_dir, "package.json"))) {
         console.log("No package.json for plugins, creating one.");
-        fs.writeFileSync(path.join(plugins_dir, "package.json"), "", "utf8");
+        fs.writeFileSync(path.join(plugins_dir, "package.json"), default_package_json, "utf8");
     }
 }
+// todo: run yarn install in plugins directory
 
 if (!doesExist(config_file)) {
     console.log("No config.json, creating one.");
     fs.writeFileSync(config_file, default_config, "utf8");
+    console.log("Created config.json at " + config_file);
 }
 
 console.log("Setup complete!");

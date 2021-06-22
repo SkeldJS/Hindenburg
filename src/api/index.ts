dbd2ba95db879373528899563165ab936d0e314e
Edward Smale <essmale2005@gmail.com>
2021-06-22 13:22:00 +0100

    Fix exports

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,5 @@
 export * from "./events";
 export * from "./hooks";
+
+export * from "./BaseReactorRpcMessage";
 export * from "./HindenburgPlugin";
--- src/packets/index.ts
+++ src/packets/index.ts
@@ -1,1 +1,2 @@
 export * from "./ModdedHelloPacket";
+export * from "./ReactorRpcMessage";

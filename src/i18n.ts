4b6b6f638c615f96fb8f11733c1958951ac0161a
Edward Smale <essmale2005@gmail.com>
2021-07-14 17:59:19 +0100

    Add portuguese (brazil) translations (closes #17)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,36 +1,45 @@
 export default {
     "havent_received_all_mods": {
-        "en_US": "The server failed to receive all of your mods, please try again"
+        "en_US": "The server failed to receive all of your mods, please try again",
+        "pt_BR": "O servidor não conseguiu receber todos os seus mods, por favor tente novamente"
     },
     "reactor_required_on_server": {
-        "en_US": "This server requires you to have Reactor, see https://reactor.gg for more information"
+        "en_US": "This server requires you to have Reactor, see https://reactor.gg for more information",
+        "pt_BR": "Este servidor requer que você tenha o Reactor, veja https://reactor.gg para mais informações"
     },
     "reactor_not_enabled_on_server": {
-        "en_US": "Reactor is not enabled on this server, please disable it"
+        "en_US": "Reactor is not enabled on this server, please disable it",
+        "pt_BR": "O Reactor não está habilitado neste servidor, por favor desabilite-o"
     },
     "mod_banned_on_server": {
         // %1 = ID of mod that is banned
-        "en_US": "Mod banned on server: %1"
+        "en_US": "Mod banned on server: %1",
+        "pt_BR": "Mod banido neste servidor: %1"
     },
     "reactor_required_for_room": {
-        "en_US": "This room requires you to have Reactor, see https://reactor.gg for more information"
+        "en_US": "This room requires you to have Reactor, see https://reactor.gg for more information",
+        "pt_BR": "Está sala requer que você tenha o Reactor, veja https://reactor.gg para mais informações"
     },
     "reactor_not_enabled_for_room": {
-        "en_US": "Reactor is not enabled for this room, please disable it"
+        "en_US": "Reactor is not enabled for this room, please disable it",
+        "pt_BR": "O Reactor não está habilitado nesta sala, por favor desabilite-o"
     },
     "missing_required_mod": {
         // %1 = ID of mod that is required
         // %2 = Version of the mod that is required
-        "en_US": "Missing required mod: %1 (%2)"
+        "en_US": "Missing required mod: %1 (%2)",
+        "pt_BR": "Mod necessário faltando: %1 (%2)"
     },
     "bad_mod_version": {
         // %1 = ID of mod that has a bad version
         // %2 = The version of the mod that the client has
         // %3 = The version of the mod that the client needs
-        "en_US": "Bad mod version for %1: %2, needs %3"
+        "en_US": "Bad mod version for %1: %2, needs %3",
+        "pt_BR": "Versão do mod ruim para %1: v%2, precisa v%3"
     },
     "mod_not_recognised": {
-        // %1 = The ID of the mod that is not allowed
-        "en_US": "Unknown mod not allowed: %1"
+        // %1 = ID of the mod that is not allowed
+        "en_US": "Unknown mod not allowed: %1",
+        "pt_BR": "Mod desconhecido não permitido: %1"
     }
 } as const;

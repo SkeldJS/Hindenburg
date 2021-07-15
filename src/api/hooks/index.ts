caf1bae940a5d3704eed371607a637e79791927b
Edward Smale <essmale2005@gmail.com>
2021-07-15 02:18:20 +0100

    Update exports

--- src/api/hooks/index.ts
+++ src/api/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from "./ChatCommand";
 export * from "./CliCommand";
 export * from "./EventListener";
+export * from "./ReactorRpcHandler";
 export * from "./MessageListener";
 export * from "./RegisterMessage";

0bec588bb59aafbad124891845ba1dd98533f0c6
Edward Smale <essmale2005@gmail.com>
2021-07-28 12:17:15 +0100

    Add broadcastToPerspective utility function

--- src/Room.ts
+++ src/Room.ts
@@ -1,10 +1,11 @@
 import { PlayerData } from "@skeldjs/core";
-import { GameSettings } from "@skeldjs/protocol";
+import { BaseGameDataMessage, GameSettings, MessageDirection } from "@skeldjs/protocol";
 import { RoomsConfig } from "./interfaces";
 
 import { Worker } from "./Worker";
 import { BaseRoom } from "./BaseRoom";
 import { Perspective } from "./Perspective";
+import { Connection } from ".";
 
 export class Room extends BaseRoom {
     playerPerspectives: Map<number, Perspective>;
@@ -44,5 +45,51 @@ export class Room extends BaseRoom {
         }
 
         return perspective;
+    }
+
+    async broadcastToPerspectives(connection: Connection, messages: BaseGameDataMessage[], reliable: boolean) {
+        const player = this.players.get(connection.clientId);
+
+        if (!player)
+            return;
+
+        for (let i = 0; i < connection.room!.activePerspectives.length; i++) {
+            const activePerspective = connection.room?.activePerspectives[i];
+
+            if (!activePerspective)
+                continue;
+
+            // get this player's player object in the perspective in question
+            const povPlayer = activePerspective.players.get(player.id);
+
+            if (!povPlayer)
+                continue;
+
+            const povNotCanceled = [];
+            for (let i = 0; i < messages.length; i++) {
+                const child = messages[i];
+
+                (child as any)._canceled = false; // reset the message's canceled state
+
+                // match the message against the perspective's incoming decoder to check whether it should get sent there
+                await activePerspective.incomingFilter.emitDecoded(child, MessageDirection.Serverbound, povPlayer);
+
+                if (child.canceled)
+                    continue;
+                    
+                // send message to the perspective
+                await activePerspective.decoder.emitDecoded(child, MessageDirection.Serverbound, connection);
+
+                if (child.canceled)
+                    continue;
+                
+                povNotCanceled.push(child);
+            }
+
+            if (povNotCanceled.length) {
+                // broadcast all messages that weren't canceled to connections in this perspective
+                await activePerspective.broadcastMessages(povNotCanceled, [], undefined, [connection], reliable);
+            }
+        }
     }
 }

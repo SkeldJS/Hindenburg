4f325f8219f9341b72c1b7c9ae76929a6ff3a612
Edward Smale <essmale2005@gmail.com>
2021-08-02 12:42:24 +0100

    Fix building

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ export * from "@skeldjs/core";
 export * from "@skeldjs/protocol";
 export * from "@skeldjs/util";
 
-// @ts-ignore typescript doesn't let you re-export RoomGameEndEvent 
+// @ts-ignore typescript doesn't let you re-export
 export * from "./api";
 export * from "./handlers";
 export * from "./interfaces";
+// @ts-ignore typescript doesn't let you re-export
 export * from "./packets";
 
 export * from "./Connection";

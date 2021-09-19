19b503b95eca36e6bfde459209e90e0d955af87b
Edward Smale <essmale2005@gmail.com>
2021-09-19 17:51:14 +0100

    Allow any event to be passed into EventListener

--- src/api/hooks/EventListener.ts
+++ src/api/hooks/EventListener.ts
@@ -3,7 +3,7 @@ import { WorkerEvents } from "../../Worker";
 
 export const hindenburgEventListenersKey = Symbol("hindenburg:events");
 
-export function EventListener<EventName extends keyof WorkerEvents>(eventName: EventName) : 
+export function EventListener<EventName extends keyof WorkerEvents>(eventName: EventName) :
     (
         target: any,
         propertyKey: string,
@@ -17,6 +17,22 @@ export function EventListener<EventName extends keyof WorkerEvents>(pluginClass:
         propertyKey: string,
         descriptor: TypedPropertyDescriptor<
             (ev: WorkerEvents[EventName]) => any
+        >
+    ) => any;
+export function EventListener(eventName: string) :
+    (
+        target: any,
+        propertyKey: string,
+        descriptor: TypedPropertyDescriptor<
+            (ev: any) => any
+        >
+    ) => any;
+export function EventListener(pluginClass: typeof Plugin, eventName: string) :
+    (
+        target: any,
+        propertyKey: string,
+        descriptor: TypedPropertyDescriptor<
+            (ev: any) => any
         >
     ) => any;
 export function EventListener<EventName extends keyof WorkerEvents>(pluginClassOrEventName: any, eventName?: any) {
@@ -36,7 +52,7 @@ export function EventListener<EventName extends keyof WorkerEvents>(pluginClassO
         if (!cachedSet) {
             Reflect.defineMetadata(hindenburgEventListenersKey, eventListeners, actualTarget);
         }
-        
+
         eventListeners.add({
             handler: descriptor.value,
             eventName: eventName || pluginClassOrEventName

00aba84632827fe24507671f591daf1dc5d1e43c
Edward Smale <essmale2005@gmail.com>
2021-10-10 14:29:05 +0100

    Refactor CLI commands

--- src/api/hooks/CliCommand.ts
+++ src/api/hooks/CliCommand.ts
@@ -1,24 +1,45 @@
 import vorpal from "vorpal";
-export const hindenburgVorpalCommand = Symbol("hindenburg:vorpalcommand");
 
-export interface VorpalCommandOptionInformation {
+import { Plugin } from "../../handlers";
+
+const hindenburgCliCommandsKey = Symbol("hindenburg:clicommands");
+
+export interface CliCommandOptionInformation {
     usage: string;
     description?: string;
 }
 
-export interface VorpalCommandInformation extends VorpalCommandOptionInformation {
-    options?: VorpalCommandOptionInformation[];
+export interface CliCommandInformation extends CliCommandOptionInformation {
+    options?: CliCommandOptionInformation[];
 }
 
-export function CliCommand(command: VorpalCommandInformation) {
+export interface PluginRegisteredCliCommandInfo {
+    handler: (args: vorpal.Args) => Promise<any>;
+    command: CliCommandInformation;
+}
+
+export function CliCommand(command: CliCommandInformation) {
     return function(
         target: any,
         propertyKey: string,
-        descriptor: TypedPropertyDescriptor<(args: vorpal.Args) => Promise<void>> // basically has to be a promise else vorpal won't complete without a callback
+        descriptor: TypedPropertyDescriptor<(args: vorpal.Args) => Promise<any>> // basically has to be a promise else vorpal won't complete without a callback
     ) {
         if (!descriptor.value)
             return;
 
-        Reflect.defineMetadata(hindenburgVorpalCommand, command, target, propertyKey);
+        const cachedSet: PluginRegisteredCliCommandInfo[]|undefined = Reflect.getMetadata(hindenburgCliCommandsKey, target);
+        const cliCommandsToRegister = cachedSet || [];
+        if (!cachedSet) {
+            Reflect.defineMetadata(hindenburgCliCommandsKey, cliCommandsToRegister, target);
+        }
+
+        cliCommandsToRegister.push({
+            handler: descriptor.value,
+            command
+        });
     };
+}
+
+export function getPluginCliCommands(pluginCtr: typeof Plugin|Plugin): PluginRegisteredCliCommandInfo[] {
+    return Reflect.getMetadata(hindenburgCliCommandsKey, pluginCtr) || [];
 }

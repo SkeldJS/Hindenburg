eb1e3545a7c9b6d8c1cb65323553e311e036088d
Edward Smale <essmale2005@gmail.com>
2021-11-06 23:58:50 +0000

    Refactor exports for correct event typings

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-export * from "@skeldjs/core";
-export * from "@skeldjs/protocol";
-export * from "@skeldjs/util";
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore typescript doesn't let you re-export
 export * from "./api";
 export * from "./handlers";
 export * from "./interfaces";
 export * from "./logger";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore typescript doesn't let you re-export
 export * from "./packets";
 
 export * from "./Connection";
 export * from "./Perspective";
 export * from "./Room";
 export * from "./Worker";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore typescript doesn't let you re-export
+export * from "@skeldjs/core";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore typescript doesn't let you re-export
+export * from "@skeldjs/protocol";
+export * from "@skeldjs/util";

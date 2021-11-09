34156f3f9a032f14200ca4e2b1260a5cb8633761
Edward Smale <essmale2005@gmail.com>
2021-11-09 18:10:02 +0000

    Fix json

--- src/i18n.ts
+++ src/i18n.ts
@@ -115,11 +115,11 @@ export default {
         "ru": "Неизвестный мод не разрешён: %1"
     },
     "invalid_quick_chat_mode_quick_chat": {
-        "en": "Invalid quick chat mode, the room expected \"Quick Chat only\" but you had \"Free or Quick chat\" enabled"
+        "en": "Invalid quick chat mode, the room expected \"Quick Chat only\" but you had \"Free or Quick chat\" enabled",
         "pt_BR": "Modo de bate-papo invalido, a sala expectava \"Bate-Papo Rápido apenas\" mas você tem \"Livre ou bate-papo Rápido\" habilitado"
     },
     "invalid_quick_chat_mode_free_chat": {
-        "en": "Invalid quick chat mode, the room expected \"Free or Quick chat\" but you had \"Quick Chat only\" enabled"
+        "en": "Invalid quick chat mode, the room expected \"Free or Quick chat\" but you had \"Quick Chat only\" enabled",
         "pt_BR": "Modo de bate-papo invalido, a sala expectava \"Livre ou bate-papo Rápido\" mas você tem \"Bate-Papo Rápido apenas\" habilitado"
     }
 } as const;

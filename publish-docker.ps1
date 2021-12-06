2ee23204e04abab5c331e11a05e66cbd237def02
Edward Smale <essmale2005@gmail.com>
2021-12-06 19:40:07 +0000

    Add powershell docker script

---
+++ publish-docker.ps1
@@ -0,0 +1,4 @@
+$tag = ((git describe --tags --abbrev=0) -join '')
+
+docker buildx create --use
+docker buildx build --push --platform linux/amd64,linux/arm/v7,linux/arm64/v8,linux/arm/v6 --tag hindenburg/hindenburg:latest --tag hindenburg/hindenburg:$tag .
--- publish-docker.sh
+++ publish-docker.sh
@@ -2,4 +2,4 @@ docker buildx build \
     --push \
     --platform linux/amd64,linux/arm/v7,linux/arm64/v8,linux/arm/v6 \
     --tag hindenburg/hindenburg:latest \
-    --tag hindenburg/hindenburg:((git describe --tags --abbrev=0) -join '').
+    --tag hindenburg/hindenburg:`git describe --tags --abbrev=0` .

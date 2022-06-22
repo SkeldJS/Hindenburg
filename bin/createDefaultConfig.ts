a3507b945e7ca2dd7584e72637a9de78f268f272
Edward Smale <essmale2005@gmail.com>
2022-06-22 19:13:14 +0100

    Update default accepted versions

--- bin/createDefaultConfig.ts
+++ bin/createDefaultConfig.ts
@@ -9,7 +9,7 @@ export function createDefaultConfig(): HindenburgConfig {
         autoUpdate: false,
         exitConfirmation: true,
         defaultLanguage: "en",
-        acceptedVersions: [ "2022.1.10.0", "2022.2.2.0" ],
+        acceptedVersions: [ "2022.4.20" ],
         matchmaker: {
             port: 22021
         },
